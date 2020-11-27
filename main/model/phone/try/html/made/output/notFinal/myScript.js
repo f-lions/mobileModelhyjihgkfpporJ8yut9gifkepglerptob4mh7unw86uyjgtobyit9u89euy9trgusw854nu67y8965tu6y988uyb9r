f727075949cd759d3e51e3aaca0b04e88e9b7ab8
f-lions <74170517+f-lions@users.noreply.github.com>
2020-11-27 15:52:31 +0530

    Create myScript.js

---
+++ main/model/phone/try/html/made/output/notFinal/myScript.js
@@ -0,0 +1,99 @@
+
+var myvar = '<!DOCTYPE html>'+
+'<html>'+
+'<head>'+
+'	<title>Model</title>'+
+'<meta name="viewport" content="width=device-width, initial-scale=1.0" http-equiv="X-UA-Compatible" content="ie=edge" />'+
+''+
+'<style>'+
+'	'+
+''+
+'	.app-launcher{'+
+'		outline: none;margin-right: 30px;width: 70px;background-color: transparent;border:0px;'+
+'	}'+
+'	.app-launcher:focus{'+
+'		opacity: 0.7;'+
+''+
+'	}'+
+'	.bottom-bar{'+
+'		position: absolute;'+
+'		left: 3%;'+
+'		top: 75%;'+
+'		opacity: 1;'+
+'background-color: white;'+
+'border-radius: 29px; height: 50px;padding:10px; '+
+'	}'+
+'</style>'+
+'</head>'+
+''+
+'<body>'+
+''+
+'	<div style="padding-top: 100px;padding-left:70px;padding-right:50px;width: 500px;background-image: url(\'file:///C:/Users/PRASHAM/Downloads/fcbgtry.svg\'); background-repeat: no-repeat;background-size: 400px;">'+
+'<svg style="position: absolute; top: 5%; right: 76%;"width="40"version="1.1" viewBox="0.0 0.0 194.5275590551181 178.10498687664042" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><clipPath id="p.0"><path d="m0 0l194.52756 0l0 178.10498l-194.52756 0l0 -178.10498z" clip-rule="nonzero"/></clipPath><g clip-path="url(#p.0)"><path fill="#000000" fill-opacity="0.0" d="m0 0l194.52756 0l0 178.10498l-194.52756 0z" fill-rule="evenodd"/><path fill="#000000" fill-opacity="0.0" d="m55.04331 121.35171l0 -64.59843l98.3307 0l0 64.59843z" fill-rule="evenodd"/><path stroke="#000000" stroke-width="12.0" stroke-linejoin="round" stroke-linecap="butt" d="m55.04331 121.35171l0 -64.59843l98.3307 0l0 64.59843z" fill-rule="evenodd"/><path fill="#000000" d="m41.15354 75.4462l13.889767 0l0 27.212593l-13.889767 0z" fill-rule="evenodd"/></g></svg><div style="overflow: scroll; height: 500px;max-height: 500px; "><button class="app-launcher"><img src="Free%20Fire-%203volution.png" style="border-radius: 40%;margin-right: 30px;"align="center" width="50">Free Fire</button><button class="app-launcher"><img src="https://preview.redd.it/9nkyndupwyj51.png?width=720&format=png&auto=webp&s=7d6557661ef771b7d35f38fdc8925411311fb6b9" style="border-radius: 40%;margin-right: 10px;"align="center" width="50"></button>'+
+'		<button class="app-launcher"><img src="https://thumbs.dreamstime.com/b/whatsapp-icon-isolated-white-vector-file-included-whatsapp-flat-icon-164609425.jpg" style="border-radius: 40%;margin-right: 10px;"align="center" width="50"></button>'+
+'		<pre>'+
+''+
+''+
+'		</pre>'+
+'			<button class="app-launcher"><img src="https://assets.stickpng.com/images/58e919b0eb97430e819064ff.png" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button><button class="app-launcher"><img src="play%20fev.png" style="border-radius: 0%;margin-right: 30px;"align="center" width="50"></button>'+
+'		<button class="app-launcher"><img src="https://lh3.googleusercontent.com/a-/AOh14GiERSPRR4hCeRQxP0ViKJfg-0DXOFBd5S0HJLxc_A=s192-c-mo" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'			<pre>'+
+''+
+''+
+'		</pre>'+
+'	'+
+'<button class="app-launcher"><img src="settings%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="clock_icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="files%20icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'	<pre>'+
+''+
+''+
+'		</pre>'+
+'	'+
+'<button class="app-launcher"><img src="settings%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="clock_icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="files%20icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>	<pre>'+
+''+
+''+
+'		</pre>'+
+'	'+
+'<button class="app-launcher"><img src="settings%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="clock_icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="files%20icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'</div>'+
+'<pre>'+
+'	'+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+''+
+'</pre>	'+
+'<div class="bottom-bar"><button class="app-launcher"><img src="file:///C:/Users/PRASHAM/Downloads/settings%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="yt_icon%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'<button class="app-launcher"><img src="g-fev_new%20(1).svg" style="border-radius: 40%;margin-right: 30px;"align="center" width="50"></button>'+
+'</div>'+
+'		</div>'+
+''+
+''+
+'</body>'+
+'</html>';
+	
